7019d6a1d7af4d2e5999903cc37c9dee203bddb2
ogoeche <eifediora@gmail.com>
2024-09-27 19:44:07 +0100

    sketching functional structure application

--- src/budget_app/main/Main.java
+++ src/budget_app/main/Main.java
@@ -1,11 +1,10 @@
-package budget_app.view;
+package budget_app.main;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import budget_app.views.CLI;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
         var cli = new CLI();
         cli.mainProcessing();
