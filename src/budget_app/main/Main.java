17b7e4a448a8fba439e85f23bf7d55c15feec7cf
ogoeche <eifediora@gmail.com>
2024-09-27 19:36:00 +0100

    first commit

---
+++ src/budget_app/main/Main.java
@@ -0,0 +1,14 @@
+package budget_app.view;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        var cli = new CLI();
+        cli.mainProcessing();
+
+    }
+}
